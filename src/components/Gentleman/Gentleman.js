f499117bec4a321d988b893af44dea0fa121438e
Marian <marianlopig@gmail.com>
2022-04-26 00:01:29 +0200

    gentlement details dinamic added

--- src/components/Gentleman/Gentleman.js
+++ src/components/Gentleman/Gentleman.js
@@ -1,26 +1,28 @@
 const Gentleman = ({ gentleman }) => {
   return (
-    <li className="gentleman" key={gentleman.id}>
+    <li className="gentleman">
       <div className="gentleman__avatar-container">
         <img
           className="gentleman__avatar"
-          src="img/fary.jpg"
-          alt="The Fary pointing at you"
+          src={"img/" + gentleman.picture}
+          alt={gentleman.alternativeText}
         />
-        <span className="gentleman__initial">F</span>
+        <span className="gentleman__initial">{gentleman.name[0]}</span>
       </div>
       <div className="gentleman__data-container">
-        <h2 className="gentleman__name">The Fary</h2>
+        <h2 className="gentleman__name">{gentleman.name}</h2>
         <ul className="gentleman__data-list">
           <li className="gentleman__data">
             <span className="gentleman__data-label">Profession:</span>
-            Influencer
+            {gentleman.profession}
           </li>
           <li className="gentleman__data">
-            <span className="gentleman__data-label">Status:</span> RIP
+            <span className="gentleman__data-label">Status:</span>
+            {gentleman.status}
           </li>
           <li className="gentleman__data">
-            <span className="gentleman__data-label">Twitter:</span> Pending
+            <span className="gentleman__data-label">Twitter:</span>
+            {gentleman.twitter}
           </li>
         </ul>
       </div>
